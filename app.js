17940886a04d2953168008283bb58e4614a381f0
khalilsediq <khalilullahsadique1334@gmail.com>
2025-12-10 16:00:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// 1. Ask the user for their age. If the age is 18 or more, show an alert: "You are an
+// adult.", else show: "You are a minor."
+///////////
+// const age = Number(prompt("Enter Your Age"));
+// if (age >= 18 && age <= 35) {
+//   alert("your are Adult, and Young");
+// } else if (age < 18 && age >= 0) {
+//   alert("you are a minor");
+// } else if (age >= 36 && age <= 55) {
+//   alert("You are Middle-aged");
+// } else if (age >= 56 && age <= 110) {
+//   alert("You are Too old, your expiry date will reach Soon!");
+// } else {
+//   alert("invalid age!");
+// }
+//////////////////Completed.
+
+
+
+
+
+
+
+
+
+
+// 2. Ask the user to enter a number. If it's positive, log: "Positive number"; else log:
+// "Not positive".
+// const userNum = Number(prompt("Enter, Number to check Positive, Negative"));
+
+// if (userNum > 0) {
+// //   alert("Postive Number");
+//   console.log("Postive Number");
+// } else if (userNum < 0) {
+// //   alert("Not Postive Number");
+//   console.log("Not a Postive Number");
+// } else if (userNum === 0) {
+// //   alert("Zero");
+//   console.log("Zero");
+// } else {
+// //   alert("NaN");
+//   console.log("NaN");
+// }
+///////////Completed...
+
+
+
+
+
