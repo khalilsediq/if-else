f4dff99f6139d62c51a13c2298f923fe352b6095
khalilsediq <khalilullahsadique1334@gmail.com>
2025-12-10 18:39:27 +0500

    Task 6 completed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 1. Ask the user for their age. If the age is 18 or more, show an alert: "You are an
 // adult.", else show: "You are a minor."
-///////////
+
 // const age = Number(prompt("Enter Your Age"));
 // if (age >= 18 && age <= 35) {
 //   alert("your are Adult, and Young");
@@ -13,7 +13,7 @@
 // } else {
 //   alert("invalid age!");
 // }
-//////////////////Completed.
+////////////////////////////////////////////////////////////////////////////////////////////////////////Completed.
 
 // 2. Ask the user to enter a number. If it's positive, log: "Positive number"; else log:
 // "Not positive".
@@ -32,7 +32,7 @@
 // //   alert("NaN");
 //   console.log("NaN");
 // }
-///////////Completed...
+///////////////////////////////////////////////////////////////////////////////////////////////////////Completed...
 
 // 3. Ask the user for a number. If it’s even, alert: "Even number"; else alert: "Odd
 // number".
@@ -46,37 +46,49 @@
 // } else {
 //   alert("Not a Valid Number");
 // }
-/////////////Completed.
+///////////////////////////////////////////////////////////////////////////////////////////////////////Completed.
 
 // 4. Ask for a username. If the username is "admin", display "Welcome, admin!"; else
 // display "Access denied."
 
 // const userName = prompt("Enter Your UserName").toUpperCase();
-
 // if (userName === "ADMIN") {
 //   alert("Wellcom, admin! Access Granted!");
 // } else {
 //   alert("Access denied");
 // }
-///////////////Completed.....
+///////////////////////////////////////////////////////////////////////////////////////////////////////Completed.....
 
 // 5. Ask for two numbers. If they are equal, log "Both numbers are the same";
 // otherwise, log "The numbers are different".
-const num1 = Number(prompt("Enter Any Number "));
-const num2 = Number(prompt("Enter Another Number"));
+// const num1 = Number(prompt("Enter Any Number "));
+// const num2 = Number(prompt("Enter Another Number"));
 
-if (num1 === num2) {
-  console.log("Both numbers are the same");
-  alert("Both numbers are the same")
-} else if (isNaN(num1) || isNaN(num2)) {
-    console.log(" ❌ Invalid Number intered ");
-    alert(" ❌ Invalid Number Entered ")
+// if (num1 === num2) {
+//   console.log("Both numbers are the same");
+//   alert("Both numbers are the same")
+// } else if (isNaN(num1) || isNaN(num2)) {
+//     console.log(" ❌ Invalid Number intered ");
+//     alert(" ❌ Invalid Number Entered ")
+// } else {
+//     console.log("The numbers are different");
+//     alert("The numbers are different")
+// }
+///////////////////////////////////////////////////////////////////////////////////////////////////////Completed......
+
+// 6. Ask the user to enter two numbers. Check which one is larger and alert: "First is
+// larger" or "Second is larger".
+
+const userNm = Number(prompt("Enter First Number"));
+const userNm2 = Number(prompt("Enter Second Number"));
+
+if (userNm > userNm2) {
+  alert("First Number Is larger than Second ");
+} else if (userNm === userNm2) {
+  alert("Both are Equal");
+} else if (isNaN(userNm) || isNaN(userNm2)) {
+  alert("Please! Enter a Valid Input");
 } else {
-    console.log("The numbers are different");
-    alert("The numbers are different")
+  alert("Second Number Is larger than First");
 }
-/////////////////////Completed......
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////Completed......
